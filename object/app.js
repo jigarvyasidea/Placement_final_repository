af086651a3a860a064e098a5397fbd4357be5870
Jigar vyas <80967728+jigarvyasidea@users.noreply.github.com>
2023-06-12 08:11:41 +0530

    Add files via upload

---
+++ object/app.js
@@ -0,0 +1,29 @@
+let person = {
+    // name : 'jigar',
+    // age :  21,
+    // isMalem : true
+    cn : 90,
+    math : 100,
+    web : 100,
+
+    Abc : function () {
+            return(90 + 100 + 100);
+    },
+
+    bulao : function() {
+        return("jigar ko bulao ");
+    }
+}
+
+
+
+
+console.log(person.Abc());
+console.log(person.bulao());
+// let person2 = person;
+
+//console.log(person2.age);
+// console.log(person);
+// console.log(person.name);
+
+//console.log(person['name']);
