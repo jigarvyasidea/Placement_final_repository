0149e4714c83c267682a0a8123aa59fe9654ee99
Jigar vyas <80967728+jigarvyasidea@users.noreply.github.com>
2023-06-12 11:31:07 +0530

    Add files via upload

--- object/app.js
+++ object/app.js
@@ -1,29 +1,48 @@
-let person = {
-    // name : 'jigar',
-    // age :  21,
-    // isMalem : true
-    cn : 90,
-    math : 100,
+// let person = {
+//     //key : val 
+//     name : 'mohit',
+//     age : 21,
+//     ismale : true
+//     //function 
+//     Abc : function(){
+
+//     }
+// }
+
+
+
+// let person2 = person;
+
+// console.log(person2.age);
+
+// console.log(person['name']);
+// console.log(person['age']);
+
+
+
+
+// console.log(person.name);
+
+
+// let arr  = [10 , 20, 30, 40];
+
+// console.log(arr[2]);
+
+
+let obj = {
+
+    cn : 50,
     web : 100,
-
-    Abc : function () {
-            return(90 + 100 + 100);
+    math : 10,
+    total : function(){
+        console.log(50 + 100 +10 )
     },
-
-    bulao : function() {
-        return("jigar ko bulao ");
+    Bulao : function(){
+        console.log("jigar ko bulao");
     }
 }
 
 
+console.log(obj.total());
+console.log(obj.bulao());
 
-
-console.log(person.Abc());
-console.log(person.bulao());
-// let person2 = person;
-
-//console.log(person2.age);
-// console.log(person);
-// console.log(person.name);
-
-//console.log(person['name']);
